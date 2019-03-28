4a661dab530cc2484bc9bbc424e199c6b5a94323
Lebed-kun <lebed-kun@mail.ru>
2019-03-28 23:19:57 +0400

    Task done

--- index.js
+++ index.js
@@ -5,5 +5,13 @@
  * @returns {String}
  */
 module.exports = function (hours, minutes, interval) {
+  minutes += hours * 60 + interval;
 
+  let hrs = Math.floor(minutes / 60) % 24;
+  hrs = (hrs < 10 ? '0' : '') + hrs;
+
+  let mins = minutes % 60;
+  mins = (mins < 10 ? '0' : '') + mins;
+
+  return `${hrs}:${mins}`;
 };
