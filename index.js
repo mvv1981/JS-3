59d5df7edbf57a57c8069b13f843e1eac70339bf
Evgenia <Engelis7865@gmail.com>
2019-03-29 23:03:07 +0400

    Сделала задание

--- index.js
+++ index.js
@@ -5,5 +5,25 @@
  * @returns {String}
  */
 module.exports = function (hours, minutes, interval) {
+    var timeInMinutes = (hours * 60) + minutes + interval;
+    hours = Math.floor(timeInMinutes / 60) % 24;
+    minutes = timeInMinutes % 60;
+    var hoursStr;
+    var minutesStr;
+    if(hours < 10){
+       hoursStr = "0" + hours;
+    }
+    else hoursStr = hours.toString();
 
+    if (minutes < 10 ){
+        minutesStr = "0" + minutes;
+    }
+    else minutesStr = minutes.toString();
+
+    return hoursStr + ":" + minutesStr;
+ 
+
+    // var d = new Date(1, 1, 1, hours, minutes + interval, 0);
+  
+    // return d.toTimeString().substring(0, 5);
 };
