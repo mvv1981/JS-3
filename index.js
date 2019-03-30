a0c5bb6658e90f678d4a02d55a5c017765f35c1f
phil11971 <phil182@mail.ru>
2019-03-30 23:33:02 +0400

    made JS-3

--- index.js
+++ index.js
@@ -4,6 +4,22 @@
  * @param {Number} interval
  * @returns {String}
  */
-module.exports = function (hours, minutes, interval) {
+module.exports = function (hours, minutes, interval) 
+{
+    if(hours>=0 && hours<=23 && minutes>=0 && minutes<=59)
+    {
+        minutes = (hours * 60) + minutes + interval;
+        hours = Math.floor(minutes / 60) % 24;
+        minutes %= 60;
+        
+        if(hours < 10){
+           hours = "0" + hours;
+        }
 
+        if (minutes < 10 ){
+            minutes = "0" + minutes;
+        }
+        
+        return hours + ":" + minutes;
+    }
 };
