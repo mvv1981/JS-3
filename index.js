2750818793a2fd9ab45afeddf6f563bf617f6e8e
Ivan <darkside8888@mail.ru>
2019-03-31 23:18:53 +0400

    JS-3 task completed [Соловьев]

--- index.js
+++ index.js
@@ -5,5 +5,7 @@
  * @returns {String}
  */
 module.exports = function (hours, minutes, interval) {
-
-};
+	let newMinutes = (minutes + interval) % 60;
+  	let newHours = (hours + Math.floor((minutes + interval)/60)) % 24;
+  	return `${(newHours / 10 > 0) ? newHours : "0" + newHours}:${(newMinutes / 10 > 0) ? newMinutes : "0" + newMinutes}`;
+  }
