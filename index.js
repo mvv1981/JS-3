c2621d595664a40dfa749114c12705e6082fbe98
Vlad Zhichkin <v.zhichkin@yandex.ru>
2019-04-03 22:53:55 +0300

    Zhichkin. Задание сделано.

--- index.js
+++ index.js
@@ -5,5 +5,19 @@
  * @returns {String}
  */
 module.exports = function (hours, minutes, interval) {
-
+    let allMinutes = hours * 60 + minutes + interval;    
+    while (allMinutes >= 1440) {
+        allMinutes = allMinutes - 1440;
+      }    
+    let newHours = Math.floor( allMinutes / 60);
+    let newMinutes = allMinutes - newHours * 60;
+    let stringHours = String(newHours);
+    let stringMinutes = String(newMinutes);
+    if (stringHours.length < 2){
+        stringHours = "0" + stringHours;
+    }
+    if (stringMinutes.length < 2){
+        stringMinutes = "0" + stringMinutes;
+    }
+    return stringHours + ":" + stringMinutes;
 };
